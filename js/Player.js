577e3b02e4be31e46d983380ccc35005e9a8aa16
janardanlatchumanan <latchumanant@gmail.com>
2020-11-08 08:43:14 +0530

    add the js folder

---
+++ js/Player.js
@@ -0,0 +1,12 @@
+class Player{
+constructor(x,y,w,h){
+this.body = Bodies.rectangle(x,y,w,h);
+World.add(world,this.body)
+this.w = w;
+this.h = h;
+}
+display(){	
+rectMode(CENTER);
+rect(this.body.position.x,this.body.position.y,this.w,this.h);	
+}
+}
